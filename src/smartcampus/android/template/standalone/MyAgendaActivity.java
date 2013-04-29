8e5faacca36e1d1ab96406b0c0770c7a31d289e7
luca.micheli <luca.micheli@1b7e5acb-c278-4105-95c9-4f87e0cd580c>
2013-04-29 07:48:03 +0000

    Skeleton of app, courses, PHL, find

--- src/smartcampus/android/template/standalone/MyAgendaActivity.java
+++ src/smartcampus/android/template/standalone/MyAgendaActivity.java
@@ -3,6 +3,8 @@ package smartcampus.android.template.standalone;
 import android.app.TabActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 
@@ -17,7 +19,7 @@ public class MyAgendaActivity extends TabActivity {
 		/** TabHost will have Tabs */
 		TabHost tabHost = getTabHost();
 		String tab1 = getResources().getString(R.string.tab_home);
-		String tab2 = getResources().getString(R.string.tab_recensioni);
+		String tab2 = getResources().getString(R.string.tab_courses);
 
 		TabSpec ts1 = tabHost.newTabSpec("tab_test1");
 		TabSpec ts2 = tabHost.newTabSpec("tab_test2");
@@ -26,6 +28,19 @@ public class MyAgendaActivity extends TabActivity {
 		tabHost.addTab(ts2.setIndicator(tab2).setContent(R.id.tab2));
 
 		tabHost.setCurrentTab(0);
+
+		String[] corsi = getResources().getStringArray(R.array.Corsi);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, corsi);
+		ListView listView = (ListView) findViewById(R.id.listViewCorsi);
+		listView.setAdapter(adapter);
+		
+		String[] corsiInt = getResources().getStringArray(R.array.CorsiInteresse);
+		
+		ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, corsiInt);
+		ListView listView2 = (ListView) findViewById(R.id.listViewCorsiInteresse);
+		listView2.setAdapter(adapter2);
 	}
 
 	@Override
--- src/smartcampus/android/template/standalone/PHLActivity.java
+++ src/smartcampus/android/template/standalone/PHLActivity.java
@@ -3,6 +3,8 @@ package smartcampus.android.template.standalone;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class PHLActivity extends Activity {
 
@@ -10,6 +12,19 @@ public class PHLActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_phl);
+		
+		String[] corsi = getResources().getStringArray(R.array.Corsi);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, corsi);
+		ListView listView = (ListView) findViewById(R.id.listViewCorsiPHL);
+		listView.setAdapter(adapter);
+		
+		String[] corsiInt = getResources().getStringArray(R.array.CorsiInteresse);
+		
+		ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, corsiInt);
+		ListView listView2 = (ListView) findViewById(R.id.listViewCorsiInteressePHL);
+		listView2.setAdapter(adapter2);
 	}
 
 	@Override
@@ -17,6 +32,7 @@ public class PHLActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.phl, menu);
 		return true;
+		
 	}
 
 }
