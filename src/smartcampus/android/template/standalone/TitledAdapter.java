e0e61b56fd0128027b3cc113678bf064503bb46f
micheli <micheli.mik@gmail.com>
2013-05-13 11:32:55 +0200

    modified adapter

--- src/smartcampus/android/template/standalone/TitledAdapter.java
+++ src/smartcampus/android/template/standalone/TitledAdapter.java
@@ -1,5 +1,6 @@
 package smartcampus.android.template.standalone;
 
+import android.R.integer;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,30 +22,42 @@ public class TitledAdapter extends ArrayAdapter<TitledItem> {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
+
 		View row = convertView;
 		TitledItem item = getItem(position);
-		
+
 		if (row == null) {
 			LayoutInflater inflater = (LayoutInflater) context
 					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			row = inflater.inflate(layoutResourceId, parent, false);
-		}			
-		
+		}
+
 		TextView title = (TextView) row.findViewById(R.id.title_text);
 		TextView content = (TextView) row.findViewById(R.id.item_text);
-		
+
 		title.setText(item.getTitle());
 		content.setText(item.getContent());
-		
+		content.setPadding(
+				(int) context.getResources().getDimension(
+						R.dimen.activity_horizontal_margin),
+				0,
+				(int) context.getResources().getDimension(
+						R.dimen.activity_horizontal_margin), 0);
 		TitledItem prev = null;
-		if (position > 0) prev = getItem(position - 1);
+		if (position > 0)
+			prev = getItem(position - 1);
 
 		if (prev == null || !(prev.getTitle().equals(item.getTitle()))) {
 			title.setVisibility(View.VISIBLE);
 		} else {
 			title.setVisibility(View.GONE);
 		}
-		
+		// row.setPadding(
+		// (int)context.getResources().getDimension(
+		// R.dimen.activity_horizontal_margin),
+		// 0,
+		// (int)context.getResources().getDimension(
+		// R.dimen.activity_horizontal_margin), 0);
 		return row;
 	}
 
