619880aca80d42050d5b0e1b9f8131fd2c908056
mattia benedetti <mattiabeta@gmail.com>
2013-06-28 14:00:38 +0200

    start development of phl section

--- src/smartcampus/android/template/standalone/PHLCorsiFragment.java
+++ src/smartcampus/android/template/standalone/PHLCorsiFragment.java
@@ -1,5 +1,7 @@
 package smartcampus.android.template.standalone;
 
+import java.util.List;
+
 import android.content.Intent;
 import android.view.View;
 import android.widget.AdapterView;
@@ -7,39 +9,47 @@ import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockListFragment;
 
-public class PHLCorsiFragment extends SherlockListFragment {
+import eu.trentorise.smartcampus.smartuni.models.CorsoLite;
 
-	public void onStart() {
+public class PHLCorsiFragment extends SherlockListFragment 
+{
+
+	public void onStart() 
+	{
 		super.onStart();
-		String[] corsi = getResources().getStringArray(R.array.Corsi);
-		String[] corsiInt = getResources().getStringArray(
-				R.array.CorsiInteresse);
-		TitledItem[] items = new TitledItem[corsi.length + corsiInt.length];
+		
+		PHLengine phl = new PHLengine();
+		List<CorsoLite> corsi_frequentati = phl.getFrequentedCourses();
+		
+		String[] corsiInt = getResources().getStringArray(R.array.CorsiInteresse);
+		TitledItem[] items = new TitledItem[corsi_frequentati.size() + corsiInt.length];
 
 		int i = 0;
-		for (String s : corsi) {
-			items[i++] = new TitledItem("Corsi da libretto", s);
+		for (CorsoLite c : corsi_frequentati) 
+		{
+			items[i++] = new TitledItem("Corsi da libretto", c.getNome());
 		}
-		for (String s : corsiInt) {
+		
+		for (String s : corsiInt) 
+		{
 			items[i++] = new TitledItem("Corsi di interesse", s);
 		}
 
 		TitledAdapter adapter = new TitledAdapter(getSherlockActivity(), items);
 		setListAdapter(adapter);
 		ListView listView = getListView();
-		listView.setOnItemClickListener(new ListView.OnItemClickListener() {
-
+		
+		listView.setOnItemClickListener(new ListView.OnItemClickListener() 
+		{
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) 
+			{
 				// TODO Auto-generated method stub String
 				Intent intent = new Intent();
 				intent.setClass(getActivity(), PHL4Courses.class);
 
 				startActivity(intent);
 			}
-
 		});
-
 	}
 }
---
+++ src/smartcampus/android/template/standalone/PHLengine.java
@@ -0,0 +1,64 @@
+package smartcampus.android.template.standalone;
+
+import java.util.List;
+
+import android.content.Context;
+
+import eu.trentorise.smartcampus.android.common.Utils;
+import eu.trentorise.smartcampus.protocolcarrier.ProtocolCarrier;
+import eu.trentorise.smartcampus.protocolcarrier.common.Constants.Method;
+import eu.trentorise.smartcampus.protocolcarrier.custom.MessageRequest;
+import eu.trentorise.smartcampus.protocolcarrier.custom.MessageResponse;
+import eu.trentorise.smartcampus.protocolcarrier.exceptions.ConnectionException;
+import eu.trentorise.smartcampus.protocolcarrier.exceptions.ProtocolException;
+import eu.trentorise.smartcampus.protocolcarrier.exceptions.SecurityException;
+import eu.trentorise.smartcampus.smartuni.models.CorsoLite;
+import eu.trentorise.smartcampus.smartuni.utilities.SmartUniDataWS;
+
+public class PHLengine
+{
+	
+	public List<CorsoLite> getFrequentedCourses() 
+	{
+
+		Context context = null;
+		ProtocolCarrier mProtocolCarrier = new ProtocolCarrier(context, SmartUniDataWS.TOKEN_NAME);
+
+		MessageRequest request = new MessageRequest( SmartUniDataWS.URL_WS_SMARTUNI, SmartUniDataWS.GET_WS_FREQUENTEDCOURSES);
+		request.setMethod(Method.GET);
+
+		MessageResponse response;
+		String body = null;
+		try 
+		{
+			response = mProtocolCarrier.invokeSync(request, SmartUniDataWS.TOKEN_NAME, SmartUniDataWS.TOKEN);
+
+			if (response.getHttpStatus() == 200) 
+			{
+				body = response.getBody();
+			}
+			else 
+			{
+				return null;
+			}
+		} 
+		catch (ConnectionException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (ProtocolException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (SecurityException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return Utils.convertJSONToObjects(body, CorsoLite.class);
+	}
+
+}
