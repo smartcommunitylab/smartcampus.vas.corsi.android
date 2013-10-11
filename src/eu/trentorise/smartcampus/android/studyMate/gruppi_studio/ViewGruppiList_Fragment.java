f6b443bac694c4f366913a4203385493eaaaa7f4
LoreBz <lolloghironf@hotmail.it>
2013-10-11 09:24:19 +0200

    first try to implement contextual menu

--- src/eu/trentorise/smartcampus/android/studyMate/gruppi_studio/ViewGruppiList_Fragment.java
+++ src/eu/trentorise/smartcampus/android/studyMate/gruppi_studio/ViewGruppiList_Fragment.java
@@ -5,18 +5,27 @@ import java.util.ArrayList;
 import smartcampus.android.template.standalone.R;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.ActionMode;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+
 import eu.trentorise.smartcampus.android.studyMate.models.GruppoDiStudio;
 
-public class ViewGruppiList_Fragment extends Fragment {
+public class ViewGruppiList_Fragment extends SherlockFragment {
 
 	private ArrayList<GruppoDiStudio> user_gds_list;
+	protected Object mActionMode;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -50,8 +59,8 @@ public class ViewGruppiList_Fragment extends Fragment {
 				/*
 				 * Per passare il contextualGDS alla overview_GDS activity,
 				 * piazzo il contextualgds nel contextualcollection e poi dalla
-				 * overview_gds vado a recuperarlo. Il contextualcollection � un
-				 * Arraylist<Object> della classe MyApplication.
+				 * overview_gds vado a recuperarlo. Il contextualcollection �
+				 * un Arraylist<Object> della classe MyApplication.
 				 * contextualcollection � statico e poich� la classe
 				 * myapplication � pubblica chiunque pu� accedere al
 				 * contextualcollection. Il contextualcollection lo uso come
@@ -69,7 +78,70 @@ public class ViewGruppiList_Fragment extends Fragment {
 			}
 		});
 
+		listview.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				if (mActionMode != null) {
+					return false;
+				}
+
+				// Start the CAB using the ActionMode.Callback defined above
+				mActionMode = ViewGruppiList_Fragment.this
+						.getSherlockActivity().startActionMode(
+								mActionModeCallback);
+				//ste tre righe sotto a cosa servono? (le ho scritte io copiando dal web ma apparentemente non fanno niente)
+				LayoutInflater inflater=getLayoutInflater(null);
+				view = inflater.inflate(R.layout.fake, null);
+				view.setSelected(true);
+				return true;
+			}
+
+		});
+
 	}
+
+	private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+
+		// Called when the action mode is created; startActionMode() was called
+		@Override
+		public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+			// Inflate a menu resource providing context menu items
+			MenuInflater inflater = mode.getMenuInflater();
+			inflater.inflate(R.menu.action_mode_allgds, menu);
+			return true;
+		}
+
+		// Called each time the action mode is shown. Always called after
+		// onCreateActionMode, but
+		// may be called multiple times if the mode is invalidated.
+		@Override
+		public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+			return false; // Return false if nothing is done
+		}
+
+		// Called when the user selects a contextual menu item
+		@Override
+		public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+			switch (item.getItemId()) {
+			case R.id.ac_elimina:
+				Toast.makeText(MyApplication.getAppContext(), "ole",
+						Toast.LENGTH_SHORT).show();
+				mode.finish(); // Action picked, so close the CAB
+				return true;
+			default:
+				return false;
+			}
+		}
+
+		// Called when the user exits the action mode
+		@Override
+		public void onDestroyActionMode(ActionMode mode) {
+			mActionMode = null;
+		}
+	};
 
 	public ArrayList<GruppoDiStudio> getUser_gds_list() {
 		return user_gds_list;
