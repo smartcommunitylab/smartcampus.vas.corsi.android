2825c9eb7ed8f549e72d7b2810273a957df03a4b
micheli <micheli.mik@gmail.com>
2014-02-18 16:44:18 +0100

    fix login (vas-dev launcher)

--- src/eu/trentorise/smartcampus/android/studyMate/start/MyUniActivity.java
+++ src/eu/trentorise/smartcampus/android/studyMate/start/MyUniActivity.java
@@ -1,6 +1,5 @@
 package eu.trentorise.smartcampus.android.studyMate.start;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -8,13 +7,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockActivity;
 
 import eu.trentorise.smartcampus.ac.AACException;
 import eu.trentorise.smartcampus.ac.SCAccessProvider;
-import eu.trentorise.smartcampus.ac.embedded.EmbeddedSCAccessProvider;
 import eu.trentorise.smartcampus.android.studyMate.finder.FindHomeActivity;
 import eu.trentorise.smartcampus.android.studyMate.gruppi_studio.Lista_GDS_activity;
 import eu.trentorise.smartcampus.android.studyMate.myAgenda.MyAgendaActivity;
@@ -34,15 +31,16 @@ public class MyUniActivity extends SherlockActivity {
 	/** Logging tag */
 	private static final String TAG = "Main";
 
-	public static final String APP_ID = "studymate";
-
-	public static final String SERVER_URL = "https://vas-dev.smartcampuslab.it/core.communicator";
+	 public static final String APP_ID = "studymate";
+	//
+	 public static final String SERVER_URL =
+	 "https://vas-dev.smartcampuslab.it/core.communicator";
 	private static Context mContext;
+	private static SCAccessProvider accessProvider = null;
 	/**
 	 * Provides access to the authentication mechanism. Used to retrieve the
 	 * token
 	 */
-	private static SCAccessProvider accessProvider = null;
 	// public static String userAuthToken;
 	public static BasicProfile bp;
 
@@ -50,33 +48,7 @@ public class MyUniActivity extends SherlockActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		mContext = getApplicationContext();
-		accessProvider = new EmbeddedSCAccessProvider();
-		try {
-			if (!accessProvider.login(this, null)) {
-				new LoadUserDataFromACServiceTask().execute();
-				// user is already registered. Proceed requesting the token
-				// and the related steps if needed
-			}
-		} catch (AACException e) {
-			Log.e(TAG, "Failed to login: " + e.getMessage());
-			// handle the failure, e.g., notify the user and close the app.
-		}
-
-	}
-
-	@Override
-	protected void onResume() {
-		try {
-			if (!accessProvider.login(this, null)) {
-				new LoadUserDataFromACServiceTask().execute();
-				// user is already registered. Proceed requesting the token
-				// and the related steps if needed
-			}
-		} catch (AACException e) {
-			Log.e(TAG, "Failed to login: " + e.getMessage());
-			// handle the failure, e.g., notify the user and close the app.
-		}
-		super.onResume();
+		new LoadUserDataFromACServiceTask().execute();
 	}
 
 	@Override
@@ -145,65 +117,16 @@ public class MyUniActivity extends SherlockActivity {
 
 	}
 
-	// @Override
-	// public boolean onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu)
-	// {
-	// getSupportMenuInflater().inflate(R.menu.my_uni, menu);
-	// return super.onCreateOptionsMenu(menu);
-	// }
-
-	// @Override
-	// public boolean onOptionsItemSelected(
-	// com.actionbarsherlock.view.MenuItem item) {
-	// switch (item.getItemId()) {
-	// case R.id.logout:
-	// new Thread(new Runnable() {
-	//
-	// @Override
-	// public void run() {
-	// try {
-	// accessProvider.logout(MyUniActivity.this);
-	//
-	// } catch (AACException e) {
-	// e.printStackTrace();
-	// }
-	//
-	// }
-	// }).start();
-	// Toast.makeText(MyUniActivity.this, "You are logged OFF!",
-	// Toast.LENGTH_SHORT).show();
-	// finish();
-	// return true;
-	// }
-	// return false;
-	// }
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// check the result of the authentication
-		if (requestCode == SCAccessProvider.SC_AUTH_ACTIVITY_REQUEST_CODE) {
-			if (resultCode == Activity.RESULT_OK) {
-				new LoadUserDataFromACServiceTask().execute();
-
-				// A&A successful. Proceed requesting the token
-				// and the related steps if needed
-
-			} else if (resultCode == Activity.RESULT_CANCELED) {
-				// Cancelled by user
-			} else {
-				// Operation failed for some reason
-				Toast.makeText(MyUniActivity.this, "Ops! c'è stato un errore!",
-						Toast.LENGTH_SHORT).show();
-			}
-		}
-		// other code here
-		super.onActivityResult(requestCode, resultCode, data);
+	public static SCAccessProvider getAccessProvider() {
+		if (accessProvider == null)
+			accessProvider = SCAccessProvider.getInstance(mContext);
+		return accessProvider;
 	}
 
 	public static String getAuthToken() throws AACException {
-		String userAuthToken;
-		userAuthToken = MyUniActivity.accessProvider.readToken(mContext);
-		return userAuthToken;
+		String mToken;
+		mToken = getAccessProvider().readToken(mContext);
+		return mToken;
 	}
 
 	public class LoadUserDataFromACServiceTask extends
@@ -213,30 +136,19 @@ public class MyUniActivity extends SherlockActivity {
 		protected BasicProfile doInBackground(Void... params) {
 			try {
 				RemoteConnector.setClientType(CLIENT_TYPE.CLIENT_ACCEPTALL);
-				String userAuthToken = getAuthToken();
-				// System.out.println(userAuthToken);
 				BasicProfileService service = new BasicProfileService(
 						"https://vas-dev.smartcampuslab.it/aac");
-				bp = service.getBasicProfile(userAuthToken);
+				bp = service.getBasicProfile(getAuthToken());
 				System.out.println(bp.getName());
 				System.out.println("USERID: " + bp.getUserId());
+				//init connector
 				PushServiceConnector connector = new PushServiceConnector();
-				// //init connector
 				try {
-					// System.out.println("token: " + userAuthToken);
-					connector.init(getApplicationContext(), userAuthToken,
+					connector.init(getApplicationContext(), getAuthToken(),
 							APP_ID, SERVER_URL);
 				} catch (CommunicatorConnectorException e) {
 					e.printStackTrace();
 				}
-
-				// try {
-				// new PushServiceConnector().init(getApplicationContext(),
-				// userAuthToken, APP_ID, SERVER_URL);
-				// } catch (CommunicatorConnectorException e) {
-				// e.printStackTrace();
-				// }
-
 				return bp;
 			} catch (Exception e) {
 				e.printStackTrace();
