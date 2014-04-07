1acd7bdcf806b793d7a97381e04af6e8da0d26b0
micheli <micheli.mik@gmail.com>
2014-04-07 13:32:46 +0200

    minor fix, add support for FBK users

--- src/eu/trentorise/smartcampus/android/studyMate/start/MyUniActivity.java
+++ src/eu/trentorise/smartcampus/android/studyMate/start/MyUniActivity.java
@@ -43,8 +43,8 @@ public class MyUniActivity extends SherlockActivity {
 
 	public static final String APP_ID = "studymate";
 	//
-	public static final String SERVER_URL = "https://vas-dev.smartcampuslab.it/core.communicator";
-	public static final String AUTH_URL = "https://vas-dev.smartcampuslab.it/aac";
+	public static final String SERVER_URL = "https://vas.smartcampuslab.it/core.communicator";
+	public static final String AUTH_URL = "https://ac.smartcampuslab.it/aac";
 	private static Context mContext;
 	private static SCAccessProvider accessProvider = null;
 	public static ProgressDialog pd;
